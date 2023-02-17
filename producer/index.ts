58c56523b5d4348ccf4334f7ce5446d07739c5e7
Marc Duiker <marcduiker@gmail.com>
2023-02-17 12:01:45 +0100

    no fixed port

--- producer/index.ts
+++ producer/index.ts
@@ -2,15 +2,16 @@ import { DaprClient } from "@dapr/dapr";
 
 // Common settings
 const daprHost = "http://localhost";
-const daprPort = "3500";
+const daprPort = process.env.DAPR_HTTP_PORT || "3500";
 
 async function main() {
     console.log("Starting...");
+
     const bindingName = "cloudflare-queues";
     const bindingOperation = "publish";
     const client = new DaprClient(daprHost, daprPort);
     for(var i = 1; i <= 10; i++) {
-        const message =  { data: "Hello World" + i };
+        const message =  { data: "Hello World " + i };
         const response = await client.binding.send(bindingName, bindingOperation, message);
         if (response)
         {
